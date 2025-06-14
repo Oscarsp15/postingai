9dcc04bba985e22f6231131a0dad51171fbc5f45
Oscarsp15 <oscar.sirlopu@gmail.com>
2025-06-14 01:15:44 -0500

    update

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def tiktok_verification():
     return render_template("tiktok-verification.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=5000)
