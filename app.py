f9aa9ebf1facfe98017bc9e1cb9afc8e1514b3ca
Oscarsp15 <oscar.sirlopu@gmail.com>
2025-06-14 02:12:22 -0500

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,send_from_directory
+from flask import Flask, render_template,redirect
 
 app = Flask(__name__)
 
@@ -17,7 +17,7 @@ def privacy():
 
 @app.route("/tiktok-verification/")
 def serve_verification_file():
-    return send_from_directory("static", "tiktokfc9Ny9rYK9nFRmkUXOJRNBZwiIph0YXc.txt")
+    return redirect("static", "tiktokfc9Ny9rYK9nFRmkUXOJRNBZwiIph0YXc.txt",code=302)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
